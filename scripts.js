41b2b50252b9cdf9b664b5d9e95773007a30ce08
Екатерина Бакалдина <e.bakaldina@city-mobil.ru>
2018-12-06 19:05:55 +0300

    add

--- scripts.js
+++ scripts.js
@@ -1,61 +1,28 @@
 
-// variables
-var $header_top = $('.header-top');
-
-
-// toggle menu
-$header_top.find('a').on('click', function() {
-    $(this).parent().toggleClass('open-menu');
-});
 
 $("#gif").hover(function() {
     var src = this.src;
     this.src = $(this).data("img");
     $(this).data("img", src);
 });
+$("#gif1").hover(function() {
+    var src = this.src;
+    this.src = $(this).data("img");
+    $(this).data("img", src);
+});
+$("#gif3").hover(function() {
+    var src = this.src;
+    this.src = $(this).data("img");
+    $(this).data("img", src);
+});
 
-// fullpage customization
 $('#fullpage').fullpage({
-    // sectionsColor: ['#B8AE9C', '#348899', '#F2AE72', '#5C832F', '#B8B89F'],
     sectionSelector: '.vertical-scrolling',
     navigation: true,
     slidesNavigation: true,
     controlArrows: false,
     anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection'],
-    menu: '#menu',
 
-    // afterLoad: function(anchorLink, index) {
-    //     $header_top.css('background', '#7000ff');
-    // },
-
-
-    afterSlideLoad: function( anchorLink, index, slideAnchor, slideIndex) {
-        if(anchorLink == 'fifthSection' && slideIndex == 1) {
-            $.fn.fullpage.setAllowScrolling(false, 'up');
-            $header_top.css('background', 'transparent');
-            $(this).css('background', '#374140');
-            $(this).find('h2').css('color', 'white');
-            $(this).find('h3').css('color', 'white');
-            $(this).find('p').css(
-                {
-                    'color': '#DC3522',
-                    'opacity': 1,
-                    'transform': 'translateY(0)'
-                }
-            );
-        }
-    },
-
-    onSlideLeave: function( anchorLink, index, slideIndex, direction) {
-        if(anchorLink == 'fifthSection' && slideIndex == 1) {
-            $.fn.fullpage.setAllowScrolling(true, 'up');
-            $header_top.css('background', 'rgba(0, 47, 77, .3)');
-        }
-    }
-});
-
-$('.slick-carousel').slick({
-    dots: true
 });
 
 function modalOpen(number) {
@@ -87,7 +54,7 @@ $(function() {
             template =
                 '<div class="trailer">'+
                 '   <div class="trailer-popup">'+
-                '       <iframe frameborder="0" src="' + url + '?autoplay=1"></iframe>'+
+                '       <iframe frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen src="' + url + '?autoplay=1"></iframe>'+
                 '   </div>'+
                 '<div class="trailer-popup-close"></div>';
 
@@ -105,3 +72,75 @@ $(function() {
     });
 });
 
+$('.slick-carousel').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    slide: '.horizontal-scroll',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    touchMove: false,
+});
+$('.slick-carousel').slick('setPosition');
+
+
+jQuery.validator.addMethod("checkMask", function(value, element) {
+    return /\+\d{1}\(\d{3}\)\d{3}-\d{4}/g.test(value);
+});
+
+$('#form').validate();
+$.validator.addClassRules({
+    'js-phone-up': {
+        checkMask: true
+    }
+});
+
+$('.js-phone-up').mask("+7(999)999-9999", {
+    autoclear: false
+});
+
+$("#form").submit(function(e){
+    e.preventDefault();
+    var phone = $('.js-phone-up').val().replace(/\D/g,'');
+    if (phone.length !== 11) {
+        return;
+    }
+
+    var tokenName = $("meta[name=csrf-token-name]").prop("content"); //
+    var token = $("meta[name=csrf-token]").prop("content");
+
+    $.get("/taxiportation", {phone: phone}, function(response) {
+        if (!response.hasOwnProperty("success") || response.success == false) {
+            if (response.hasOwnProperty('errors') && response.errors.hasOwnProperty('msg')){
+                $(".modal-text").html(response.errors.msg);
+                ga('send', 'event', 'SMS_landing', 'not_send', response.errors.msg);
+            } else {
+                $(".modal-text").html("Произошла ошибка");
+            }
+        } else {
+            $(".modal-text").html("Ссылка для скачивания была отправлена на ваш номер телефона");
+            ga('send', 'event', 'SMS_landing', 'send');
+        }
+
+        var openbutton = document.querySelector('.js-OpenModal');
+        var modalwindow = document.querySelector('.modals');
+        var closebutton = document.querySelector('.modals .close');
+        var modalsOverley = document.querySelector('.modals__overlay');
+        modalwindow.classList.add("show");
+        var closebutton = document.querySelector('.modals .close');
+        var modalsOverley = document.querySelector('.modals__overlay');
+        modalsOverley.onclick = function() {
+            modalwindow.classList.remove("show");
+        }
+        closebutton.onclick = function() {
+            modalwindow.classList.remove("show");
+        }
+    });
+});
+
+$(".block-1").on("click","a", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+        left = $(id).offset().left;
+    $('body,html').animate({scrollBottom: bottom}, 1500);
+});
